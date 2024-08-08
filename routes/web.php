69ba4dc3214fcc63991b6b97e4a4d40987402ac4
Colin Osborn <colin.osborn@cofense.com>
2024-08-07 20:09:39 -0600

    implemented home controller changes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::middleware('auth', 'verified')->group(function() {
+Route::middleware(['auth', 'verified'])->group(function() {
     Route::get('/', [HomeController::class, 'home'])->name('dashboard');
 });
 
